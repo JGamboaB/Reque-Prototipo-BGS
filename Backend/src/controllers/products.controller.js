6ea718641c68b82f4eb6440b4ea709ac2056f1e7
Orantoon <71191565+Orantoon@users.noreply.github.com>
2021-11-14 20:40:23 -0600

    Backend TERMINAR

---
+++ Backend/src/controllers/products.controller.js
@@ -0,0 +1,9 @@
+import {getConnection} from "../database/connection";
+
+export const getProducts = async (req, res) => {
+    const pool = await getConnection();
+    const result = await pool.request().query('SELECT * FROM Products');
+    console.log(result);
+    
+    res.json(result.recordset);
+}
